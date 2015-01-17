01938a08b3d4aae7a161e19efe4fd0d8ca5f1ded
Will <will@glozer.net>
2015-01-17 15:40:23 +0900

    call sodium & openssl version functions

--- src/version.c
+++ src/version.c
@@ -2,18 +2,18 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <openssl/opensslv.h>
+#include <openssl/crypto.h>
 #include <sodium/version.h>
 #include "version.h"
 
 const char *keys_version() {
     static char version[1024];
     const  char *keys   = "keys "   KEYS_VERSION;
-    const  char *sodium = "sodium " SODIUM_VERSION_STRING;
-    const  char *ssl    = OPENSSL_VERSION_TEXT;
+    const  char *sodium = sodium_version_string();
+    const  char *ssl    = SSLeay_version(SSLEAY_VERSION);
 
     int len = strchr(strchr(ssl, ' ') + 1, ' ') - ssl;
-    snprintf(version, sizeof(version), "%s / %.*s / %s", keys, len, ssl, sodium);
+    snprintf(version, sizeof(version), "%s / %.*s / sodium %s", keys, len, ssl, sodium);
 
     return version;
 }
